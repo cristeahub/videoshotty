a68016590c7420d687e4ae53874775a7ae4dca93
Aleksander Vognild Burkow <aleksanderburkow@gmail.com>
2016-05-26 13:59:31 +0200

    Add button for activation

--- data/capture.js
+++ data/capture.js
@@ -1,25 +1,39 @@
-var video = document.querySelectorAll("video")[0];
-
 var TARGET_WIDTH = 640;
-var scale = TARGET_WIDTH / video.clientWidth;
 
-var canvas = document.createElement("canvas");
-var ctx = canvas.getContext("2d");
-canvas.width  = video.clientWidth * scale;
-canvas.height = video.clientHeight * scale;
-
-document.body.appendChild(canvas);
-
-var FRAMES_TO_CAPTURE = 60 * 10;
-var frames = [];
-frames.length = FRAMES_TO_CAPTURE;
-
-var current_frame = 0;
-requestAnimationFrame(function renderLoop() {
-  ctx.drawImage(video, 0, 0, canvas.width, canvas.height);
-  var imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
-  frames[current_frame % FRAMES_TO_CAPTURE] = imageData;
-  current_frame += 1;
-
-  requestAnimationFrame(renderLoop);
+self.port.on("bindListeners", function(tag) {
+  var videoElements = document.querySelectorAll("video");
+  [].forEach.call(videoElements, function(element) {
+    bindGifferToVideoElement(element);
+  });
 });
+
+function bindGifferToVideoElement(videoElement) {
+  var scale = TARGET_WIDTH / videoElement.clientWidth;
+
+  var canvas = document.createElement("canvas");
+  var ctx = canvas.getContext("2d");
+  canvas.width  = videoElement.clientWidth * scale;
+  canvas.height = videoElement.clientHeight * scale;
+
+  var FRAMES_TO_CAPTURE = 60 * 10;
+  var frames = [];
+  frames.length = FRAMES_TO_CAPTURE;
+
+  var current_frame = 0;
+  requestAnimationFrame(function renderLoop() {
+    ctx.drawImage(videoElement, 0, 0, canvas.width, canvas.height);
+    var imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
+    frames[current_frame % FRAMES_TO_CAPTURE] = imageData;
+    current_frame += 1;
+
+    requestAnimationFrame(renderLoop);
+  });
+
+  var element = document.createElement('div');
+  element.textContent = "KNAPP";
+  element.onClick = function() {
+    console.log("STUFUAIFOUAOIFU");
+  };
+
+  document.body.appendChild(element);
+}
--- index.js
+++ index.js
@@ -1,6 +1,20 @@
 var buttons = require('sdk/ui/button/action');
 var pageMod = require("sdk/page-mod");
 
+worker = null
+
+pageMod.PageMod({
+  include: '*',
+  contentScriptFile: [
+    "./capture.js", "./textOverlay.js", "./conversion.js",
+    "./gif.js", "./gif.worker.js", "./modal.js"]
+  onAttach: function(_worker) {
+    // Apparently there's one of these per tab,
+    // so should probably be stored in a hashmap or something
+    worker = _worker
+  }
+});
+
 var button = buttons.ActionButton({
   id: "videoshotty-toolbar-button",
   label: "Videoshotty!",
@@ -9,11 +23,7 @@ var button = buttons.ActionButton({
     "32": "./videoshotty-32.png",
     "64": "./videoshotty-64.png"
   },
-});
-
-pageMod.PageMod({
-  include: '*',
-  contentScriptFile: [
-    "./capture.js", "./textOverlay.js", "./conversion.js",
-    "./gif.js", "./gif.worker.js", "./modal.js"]
+  onClick: function() {
+    worker.port.emit("bindListeners");
+  }
 });
