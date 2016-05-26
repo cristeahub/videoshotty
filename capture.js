578fb396c22839a362cdf58f46596fe7640bbacf
Aleksander Vognild Burkow <aleksanderburkow@gmail.com>
2016-05-26 11:12:44 +0200

    Add ring buffer to frame capture

--- capture.js
+++ capture.js
@@ -10,9 +10,16 @@ canvas.height = height;
 
 document.body.appendChild(canvas);
 
+var FRAMES_TO_CAPTURE = 60 * 10;
+var frames = [];
+frames.length = FRAMES_TO_CAPTURE;
+
+var current_frame = 0;
 requestAnimationFrame(function renderLoop() {
   ctx.drawImage(video, 0, 0, width, height);
   var imageData = ctx.getImageData(0, 0, width, height);
+  frames[current_frame % FRAMES_TO_CAPTURE] = imageData;
+  current_frame += 1;
 
   requestAnimationFrame(renderLoop);
 });
