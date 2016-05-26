d4fe8db601de60da471035ae346bffd5f5bca9b4
Christoffer Tønnessen <christoffer.tonnessen@gmail.com>
2016-05-26 14:51:53 +0200

    Add file uploading

--- data/conversion.js
+++ data/conversion.js
@@ -8,12 +8,17 @@ function createGifFromImageData(data) {
     gif.addFrame(data[i]);
   };
 
-  gif.on('finished', function() {
-    console.log('we are done creating your gif');
+  gif.on('finished', function(blob) {
+    var xhr = new XMLHttpRequest();
+    xhr.open('POST', '//54.229.214.117/upload', true);
+    xhr.onload = function(e) {
+      console.log('uploading');
+    };
+    xhr.send(blob);
   });
 
   gif.on('progress', function(p) {
-    console.log(p);
+    console.log('rendering' + p);
   });
 
   gif.render();
